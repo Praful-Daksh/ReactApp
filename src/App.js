24fd993cf5fe58125fe57fbba57b552302d86957
Praful-Daksh <prafuldaksh@proton.me>
2025-02-03 21:12:09 +0530

    json server

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import AddTask from './components/AddTask'
 
 function App() {
   const [showAddTask,setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [{
-      id: 1,
-      text: 'Doctor Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Football Match',
-      day: 'Feb 6th at 4:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Shopping',
-      day: 'Feb 9th at 10:30pm',
-      reminder: false,
-    },
-    ])
+  const [tasks, setTasks] = useState([])
+  
+  //use Effect
+  useEffect(()=>{
+    const fetchTasks = async ()=>{
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      console.log(data)
+    }
+    fetchTasks()
+  }, [])
 
   //Delete Task
   const deleteTask = (id) => {
